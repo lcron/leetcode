f7025bbfac87fbaa86aebbfea03e848be77931d9
lcron <rongbig@qq.com>
2019-05-20 02:45:27 +0800

    leetcode #1

--- aux.js
+++ aux.js
@@ -1,6 +1,11 @@
 
 // usage: require("<file>")()
 module.exports = function(){
+print=function(){
+  for (var i=0; i<arguments.length;i++)
+  process.stdout.write(arguments[i]+" ")
+}
+
 // 对象深比较
 eq=function(a,b) {
   var res = a === b
@@ -53,49 +58,103 @@ test_quiet = (proc,input,expect)=>{
   res.pass = eq(res.output,expect)
   return res
 }
+/*
 
-test=(proc,input,expect)=>{
+
+0 全部不显示
+1 只显示结果
+2 同 1，不通过时显示实际输出和预期输出
+3 同 2，不通过时显示输入
+4 同 2，始终显示输入，默认
+5 同 4，始终显示输出和预期
+*/
+test=(proc,input,expect,loglv)=>{
+  loglv = typeof loglv == "number"?loglv:4
+  var msg = {
+    input:"",
+    output:"",
+    res:""
+  }
   input.forEach(function(v,i){
-    console.log("[input "+i+"]  " + v)
+    msg.input+="[input "+i+"]  " + v+"\n"
   })
-
+  
   var res = test_quiet(proc,input,expect)
 
+  msg.res = loglv > 0?((res.pass?"PASS":"FAIL")+" in "+res.time+" ms \n"):""
+  msg.output = loglv>4 || (loglv > 1 && !res.pass)?( " - res: "+res.output+"\n - exp: "+expect+"\n"):""
+  msg.input = (res.pass&&loglv>2) || loglv > 3?msg.input:""
+  var _msg = msg.input+msg.output+msg.res
+  if(_msg)
+    console.log(_msg)
+/*
+  
   console.log("%s in %d ms\n%s",res.pass?"PASS":"FAIL",res.time,res.pass?"":("exp: "+expect+"\nres: "+res.output+"\n"))
+*/
   return res
 }
-
-tests = function(proc,cases,count){
+/*
+0 全部不显示
+1 只显示总结果
+2 同1，列出不通过的测试用例，
+3 同2，完成一次后输出结果 默认
+4+ test loglv-2 如果cases是数组时默认
+...7
+*/
+tests = function(proc,cases,count,loglv){
   var arr = false
   if(cases instanceof Array){
+    count = cases.length
+    loglv = typeof loglv == "number"?loglv:6
     arr = true
     _cases = cases
-    cases = (i)=>{return _cases[i-1]}
+    cases = (i)=>{return _cases[i]}
+  }else{
+    loglv = typeof loglv == "number"?loglv:3
+    count = count || 100
   }
   
   console.log("==== Start =============")
-  var i,res,pass=0
-  timer.start()
-  for(i=1;true;i++){
+  var i,_res,res={
+    pass:0,
+    fail:0,
+    time:0,
+    percent:0
+  }
+  var fail_msg = "---- FAILED ------------\n"
+  for(i=0;i<count;i++){
     var _case = cases(i)
     if(!_case){
       break
     }
-    if (arr)
-      console.log("Test case "+i+": ")
-    res = (arr?test: test_quiet)(proc,_case[0],_case[1])
-    
-   pass += res.pass?1:0
-    
+    if (loglv > 2){
+      console.log("Test case ["+i+"] : ")
+    }
+    _res = test(proc,_case[0],_case[1],loglv-2)
+
+    res.time += _res.time
+    if(_res.pass){
+      res.pass ++
+    }else{
+      res.fail ++
+      _case[0].forEach(function(v,i){
+    fail_msg+="[input "+i+"]  " + v+"\n"
+      })
+    fail_msg+=" res: "+_res.output+"\n exp: "+ _case[1]+"\n"
+    }
+
   } // for i
 
-  --i
-  var time = timer.end()
+  
+  
+  //console.log(res)
   var msg = "---- RESULT ------------\n"+
-  "Test "+i+" cases in "+time+" ms.\n "+
-  pass+" pass, "+ (i-pass)+" fail. \t"+(pass*100/i)+"%\n"
+  "Test "+i+" cases in "+res.time+" ms.\n "+
+  res.pass+" pass, "+ (i-res.pass)+" fail. \t"+(res.pass*100/i)+"%\n"
+  console.log(loglv>1?fail_msg:"")
   console.log(msg)
   
+  return res
 } // tests
 
 // 生成器
@@ -111,7 +170,7 @@ rand = (max,min)=>{
   return Math.floor(Math.random()*(max+1-min)+min)
 }
 rand_array =(len,max,min,only)=>{
-  max = max || 0x7FFFFFFF
+  max = max || max == 0?0:0x7FFFFFFF
   min = min || 0
   if(max < min){
     var tmp = max
--- src/tow_sum.js
+++ src/tow_sum.js
@@ -27,14 +27,20 @@ tests(proc,cases)
 
 function randcase(level){
   return function(i){
-     
-    return null
+    var a = rand_array(level)
+    var expect = rand_array(2,0,level-1,true).sort((a,b)=>a-b)
+    
+    return [[a,a[expect[0]]+a[expect[1]]],expect]
   }
 }
 
-tests(proc,randcase(100),1000)
+tests(proc,randcase(10),10)
 
+console.log("level: ",100)
+tests(proc,randcase(100),10,2)
 
+console.log("level: ",1000)
+tests(proc,randcase(1000),10,2)
 
-
-
+console.log("level: ",10000)
+tests(proc,randcase(10000),10,2)
